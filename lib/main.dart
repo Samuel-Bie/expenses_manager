7d15ab76ee97c7949d028e23028483e63cc4976a
Samuel Bie <samuelbiezinho@gmail.com>
2020-07-11 17:12:24 +0200

    Basic structure of card

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,18 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
-    Transaction(id:'t1', title: "New Shoes",          amount:15.55, date: DateTime.now()),
-    Transaction(id:'t2', title: "New Book",           amount:152.02, date: DateTime.now()),
-    Transaction(id:'t3', title: "New MBP",            amount:15.22, date: DateTime.now()),
-    Transaction(id:'t4', title: "Party",              amount:15.26, date: DateTime.now()),
-    Transaction(id:'t5', title: "Chill with friends", amount:95, date: DateTime.now()),
+    Transaction(
+        id: 't1', title: "New Shoes", amount: 15.55, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: "New Book", amount: 152.02, date: DateTime.now()),
+    Transaction(
+        id: 't3', title: "New MBP", amount: 15.22, date: DateTime.now()),
+    Transaction(id: 't4', title: "Party", amount: 15.26, date: DateTime.now()),
+    Transaction(
+        id: 't5',
+        title: "Chill with friends",
+        amount: 95,
+        date: DateTime.now()),
   ];
 
   @override
@@ -36,8 +43,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Expenses manager'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -46,13 +51,26 @@ class MyHomePage extends StatelessWidget {
               child: Text('CHART'),
             ),
           ),
-
           Column(
-            children: transactions.map(
-              (transaction) {
-                return Card(child: Text(transaction.title),);
-              }
-            ).toList(),
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(tx.amount.toString()),
+                    ),
+                    SizedBox(width: 10,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
